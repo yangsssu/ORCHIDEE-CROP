10aacb4ab9b7278c82573e9bb1e6ae1056f7fca6
Yang Su <66619180+yangsssu@users.noreply.github.com>
2023-07-24 23:49:52 +0200

    Create 1_create_and_sub_all_jobs_and_collect_the_data_after_calculation.sh

---
+++ Config/ORCHIDEE_OL/Case-2-modified-algorithm/Calculation-1-MW-dynamic-rotation-at-future-NW-RCP85-climate-1_5/1_create_and_sub_all_jobs_and_collect_the_data_after_calculation.sh
@@ -0,0 +1,70 @@
+for i in {1..841}
+
+do
+
+cd /home/orchidee04/yangsu/IGCM_OUT/OL2/DEVT/Case-2-modified-algorithm/Calculation-future-NW-RCP85-1_5
+
+rm -rf Pixel-$i
+
+cd /home/orchidee04/yangsu/Calibration/0_a_Final_calibration/Config/ORCHIDEE_OL/Case-2-modified-algorithm/Calculation-1-MW-dynamic-rotation-at-future-NW-RCP85-climate-1_5
+
+rm -rf Pixel-$i
+
+cd /home/orchidee04/yangsu/Calibration/0_a_Final_calibration/Config/ORCHIDEE_OL/Case-2-modified-algorithm/Calculation-1-MW-dynamic-rotation-at-future-NW-RCP85-climate-1_5
+
+mkdir Pixel-$i
+
+cp -r Pixel-0/* Pixel-$i/
+
+cd /home/orchidee04/yangsu/Calibration/0_a_Final_calibration/Config/ORCHIDEE_OL/Case-2-modified-algorithm/Calculation-1-MW-dynamic-rotation-at-future-NW-RCP85-climate-1_5/Pixel-$i/
+
+rm -rf run.card run.card.bak Debug Script*
+
+rm -f Job_* run.card.init
+
+cd /home/orchidee04/yangsu/Other_datasets/c_Final_calculation_case_2/Year_future-NW_RCP85_1_5
+
+cp config.card.$i.txt /home/orchidee04/yangsu/Calibration/0_a_Final_calibration/Config/ORCHIDEE_OL/Case-2-modified-algorithm/Calculation-1-MW-dynamic-rotation-at-future-NW-RCP85-climate-1_5/Pixel-$i/config.card
+
+cd /home/orchidee04/yangsu/Calibration/0_a_Final_calibration/Config/ORCHIDEE_OL/Case-2-modified-algorithm/Calculation-1-MW-dynamic-rotation-at-future-NW-RCP85-climate-1_5/Pixel-$i/
+
+../../../../../libIGCM/ins_job
+
+cd /home/orchidee04/yangsu/Calibration/0_a_Final_calibration/Config/ORCHIDEE_OL/Case-2-modified-algorithm/Calculation-1-MW-dynamic-rotation-at-future-NW-RCP85-climate-1_5/Pixel-$i/
+
+cp Job_Pixel-$i Job_Pixel-$i.txt
+
+sed '71s/.*/PeriodNb=11/' Job_Pixel-$i.txt > new_Job_Pixel-$i.txt
+
+cp new_Job_Pixel-$i.txt Job_Pixel-$i
+
+rm -f Job_Pixel-$i.txt new_Job_Pixel-$i.txt
+
+cp Job_Pixel-$i Job_Pixel-$i.txt
+
+sed '7s/.*/#PBS -q medium/' Job_Pixel-$i.txt > new_Job_Pixel-$i.txt
+
+cp new_Job_Pixel-$i.txt Job_Pixel-$i
+
+cd /home/orchidee04/yangsu/Calibration/0_a_Final_calibration/Config/ORCHIDEE_OL/Case-2-modified-algorithm/Calculation-1-MW-dynamic-rotation-at-future-NW-RCP85-climate-1_5/Pixel-$i/PARAM
+
+rm -f run.def
+
+cd /home/orchidee04/yangsu/Other_datasets/c_Final_calculation_case_2/Year_future-NW_RCP85_1_5
+
+cp run$i.def.txt /home/orchidee04/yangsu/Calibration/0_a_Final_calibration/Config/ORCHIDEE_OL/Case-2-modified-algorithm/Calculation-1-MW-dynamic-rotation-at-future-NW-RCP85-climate-1_5/Pixel-$i/PARAM/run.def
+
+cd /home/orchidee04/yangsu/Calibration/0_a_Final_calibration/Config/ORCHIDEE_OL/Case-2-modified-algorithm/Calculation-1-MW-dynamic-rotation-at-future-NW-RCP85-climate-1_5/Pixel-$i/
+
+qsub -m n Job_Pixel-$i
+
+rm -f new_Job_*
+
+rm -f Job_Pixel-*.txt
+
+done
+
+qstat
+
+
+
