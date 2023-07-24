06ba3a5ba658f8577ecdc6daf8fa5d17bb69fe35
Yang Su <66619180+yangsssu@users.noreply.github.com>
2023-07-24 21:43:28 +0200

    Add files via upload

---
+++ Config/ORCHIDEE_OL/Case-2-modified-algorithm/Calculation-1-MW-dynamic-rotation-at-current-NW-RCP85-climate-1_5/3_collect_data.sh
@@ -0,0 +1,57 @@
+for k in {1..841}
+
+do
+
+cd /home/orchidee04/yangsu/IGCM_OUT/OL2/DEVT/Case-2-modified-algorithm/Calculation-current-NW-RCP85-1_5/Pixel-$k/SBG/Output/MO
+
+for n in `ls|head -1`; do cp $n /home/orchidee04/yangsu/IGCM_OUT/OL2/DEVT/Case-2-modified-algorithm/Calculation-current-NW-RCP85-1_5/Final-total_pixel-year-1; done
+
+for n in `ls|head -2|tail -1`; do cp $n /home/orchidee04/yangsu/IGCM_OUT/OL2/DEVT/Case-2-modified-algorithm/Calculation-current-NW-RCP85-1_5/Final-total_pixel-year-1_5; done
+
+for n in `ls|head -3|tail -1`; do cp $n /home/orchidee04/yangsu/IGCM_OUT/OL2/DEVT/Case-2-modified-algorithm/Calculation-current-NW-RCP85-1_5/Final-total_pixel-year-3; done
+
+for n in `ls|head -4|tail -1`; do cp $n /home/orchidee04/yangsu/IGCM_OUT/OL2/DEVT/Case-2-modified-algorithm/Calculation-current-NW-RCP85-1_5/Final-total_pixel-year-3_5; done
+
+for n in `ls|head -5|tail -1`; do cp $n /home/orchidee04/yangsu/IGCM_OUT/OL2/DEVT/Case-2-modified-algorithm/Calculation-current-NW-RCP85-1_5/Final-total_pixel-year-5; done
+
+for n in `ls|head -6|tail -1`; do cp $n /home/orchidee04/yangsu/IGCM_OUT/OL2/DEVT/Case-2-modified-algorithm/Calculation-current-NW-RCP85-1_5/Final-total_pixel-year-5_5; done
+
+for n in `ls|head -7|tail -1`; do cp $n /home/orchidee04/yangsu/IGCM_OUT/OL2/DEVT/Case-2-modified-algorithm/Calculation-current-NW-RCP85-1_5/Final-total_pixel-year-7; done
+
+for n in `ls|head -8|tail -1`; do cp $n /home/orchidee04/yangsu/IGCM_OUT/OL2/DEVT/Case-2-modified-algorithm/Calculation-current-NW-RCP85-1_5/Final-total_pixel-year-7_5; done
+
+for n in `ls|head -9|tail -1`; do cp $n /home/orchidee04/yangsu/IGCM_OUT/OL2/DEVT/Case-2-modified-algorithm/Calculation-current-NW-RCP85-1_5/Final-total_pixel-year-9; done
+
+for n in `ls|head -10|tail -1`; do cp $n /home/orchidee04/yangsu/IGCM_OUT/OL2/DEVT/Case-2-modified-algorithm/Calculation-current-NW-RCP85-1_5/Final-total_pixel-year-9_5; done
+
+for n in `ls|head -11|tail -1`; do cp $n /home/orchidee04/yangsu/IGCM_OUT/OL2/DEVT/Case-2-modified-algorithm/Calculation-current-NW-RCP85-1_5/Final-total_pixel-year-11; done
+
+for n in `ls|head -12|tail -1`; do cp $n /home/orchidee04/yangsu/IGCM_OUT/OL2/DEVT/Case-2-modified-algorithm/Calculation-current-NW-RCP85-1_5/Final-total_pixel-year-11_5; done
+
+cd /home/orchidee04/yangsu/IGCM_OUT/OL2/DEVT/Case-2-modified-algorithm/Calculation-current-NW-RCP85-1_5/Pixel-$k/SRF/Output/MO
+
+for n in `ls|head -1`; do cp $n /home/orchidee04/yangsu/IGCM_OUT/OL2/DEVT/Case-2-modified-algorithm/Calculation-current-NW-RCP85-1_5/Final-total_pixel-year-1; done
+
+for n in `ls|head -2|tail -1`; do cp $n /home/orchidee04/yangsu/IGCM_OUT/OL2/DEVT/Case-2-modified-algorithm/Calculation-current-NW-RCP85-1_5/Final-total_pixel-year-1_5; done
+
+for n in `ls|head -3|tail -1`; do cp $n /home/orchidee04/yangsu/IGCM_OUT/OL2/DEVT/Case-2-modified-algorithm/Calculation-current-NW-RCP85-1_5/Final-total_pixel-year-3; done
+
+for n in `ls|head -4|tail -1`; do cp $n /home/orchidee04/yangsu/IGCM_OUT/OL2/DEVT/Case-2-modified-algorithm/Calculation-current-NW-RCP85-1_5/Final-total_pixel-year-3_5; done
+
+for n in `ls|head -5|tail -1`; do cp $n /home/orchidee04/yangsu/IGCM_OUT/OL2/DEVT/Case-2-modified-algorithm/Calculation-current-NW-RCP85-1_5/Final-total_pixel-year-5; done
+
+for n in `ls|head -6|tail -1`; do cp $n /home/orchidee04/yangsu/IGCM_OUT/OL2/DEVT/Case-2-modified-algorithm/Calculation-current-NW-RCP85-1_5/Final-total_pixel-year-5_5; done
+
+for n in `ls|head -7|tail -1`; do cp $n /home/orchidee04/yangsu/IGCM_OUT/OL2/DEVT/Case-2-modified-algorithm/Calculation-current-NW-RCP85-1_5/Final-total_pixel-year-7; done
+
+for n in `ls|head -8|tail -1`; do cp $n /home/orchidee04/yangsu/IGCM_OUT/OL2/DEVT/Case-2-modified-algorithm/Calculation-current-NW-RCP85-1_5/Final-total_pixel-year-7_5; done
+
+for n in `ls|head -9|tail -1`; do cp $n /home/orchidee04/yangsu/IGCM_OUT/OL2/DEVT/Case-2-modified-algorithm/Calculation-current-NW-RCP85-1_5/Final-total_pixel-year-9; done
+
+for n in `ls|head -10|tail -1`; do cp $n /home/orchidee04/yangsu/IGCM_OUT/OL2/DEVT/Case-2-modified-algorithm/Calculation-current-NW-RCP85-1_5/Final-total_pixel-year-9_5; done
+
+for n in `ls|head -11|tail -1`; do cp $n /home/orchidee04/yangsu/IGCM_OUT/OL2/DEVT/Case-2-modified-algorithm/Calculation-current-NW-RCP85-1_5/Final-total_pixel-year-11; done
+
+for n in `ls|head -12|tail -1`; do cp $n /home/orchidee04/yangsu/IGCM_OUT/OL2/DEVT/Case-2-modified-algorithm/Calculation-current-NW-RCP85-1_5/Final-total_pixel-year-11_5; done
+
+done
