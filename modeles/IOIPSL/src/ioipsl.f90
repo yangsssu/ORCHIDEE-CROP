894f09b1bf9e367712fece7bae7a5d673a5e7958
Yang Su <66619180+yangsssu@users.noreply.github.com>
2023-07-25 00:17:09 +0200

    Add files via upload

---
+++ modeles/IOIPSL/src/ioipsl.f90
@@ -0,0 +1,17 @@
+MODULE ioipsl
+!
+!$Id: ioipsl.f90 386 2008-09-04 08:38:48Z bellier $
+!-
+! This software is governed by the CeCILL license
+! See IOIPSL/IOIPSL_License_CeCILL.txt
+!
+  USE errioipsl 
+  USE stringop
+  USE mathelp    
+  USE getincom
+  USE calendar   
+  USE fliocom    
+  USE flincom    
+  USE histcom    
+  USE restcom
+END MODULE ioipsl
