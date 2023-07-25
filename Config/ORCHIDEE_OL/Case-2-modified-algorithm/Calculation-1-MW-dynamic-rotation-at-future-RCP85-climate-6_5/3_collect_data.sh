e75af0d739136fd558fdb5752b204fc278119106
Yang Su <66619180+yangsssu@users.noreply.github.com>
2023-07-25 10:46:05 +0200

    Add files via upload

---
+++ Config/ORCHIDEE_OL/Case-2-modified-algorithm/Calculation-1-MW-dynamic-rotation-at-future-RCP85-climate-6_5/3_collect_data.sh
@@ -0,0 +1,13 @@
+for k in {1..841}
+
+do
+
+cd /home/orchidee04/yangsu/IGCM_OUT/OL2/DEVT/Case-2-modified-algorithm/Calculation-future-RCP85-6_5/Pixel-$k/SBG/Output/MO
+
+for n in `ls|tail -1`; do cp $n /home/orchidee04/yangsu/IGCM_OUT/OL2/DEVT/Case-2-modified-algorithm/Calculation-future-RCP85-6_5/Final-total_pixel-year-6_5; done
+
+cd /home/orchidee04/yangsu/IGCM_OUT/OL2/DEVT/Case-2-modified-algorithm/Calculation-future-RCP85-6_5/Pixel-$k/SRF/Output/MO
+
+for n in `ls|tail -1`; do cp $n /home/orchidee04/yangsu/IGCM_OUT/OL2/DEVT/Case-2-modified-algorithm/Calculation-future-RCP85-6_5/Final-total_pixel-year-6_5; done
+
+done
