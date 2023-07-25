0eef13eacdd602e7cf937506fe4dec83e760907a
Yang Su <66619180+yangsssu@users.noreply.github.com>
2023-07-25 08:54:40 +0200

    Add files via upload

---
+++ Config/ORCHIDEE_OL/Case-2-modified-algorithm/Calculation-1-MW-dynamic-rotation-at-current-NW-RCP85-climate-5_5/3_collect_data.sh
@@ -0,0 +1,17 @@
+for k in {1..841}
+
+do
+
+cd /home/orchidee04/yangsu/IGCM_OUT/OL2/DEVT/Case-2-modified-algorithm/Calculation-current-NW-RCP85-5_5/Pixel-$k/SBG/Output/MO
+
+for n in `ls|tail -1`; do cp $n /home/orchidee04/yangsu/IGCM_OUT/OL2/DEVT/Case-2-modified-algorithm/Calculation-current-NW-RCP85-5_5/Final-total_pixel-year-5_5; done
+
+for n in `ls|head -1`; do cp $n /home/orchidee04/yangsu/IGCM_OUT/OL2/DEVT/Case-2-modified-algorithm/Calculation-current-NW-RCP85-5_5/Final-total_pixel-year-5; done
+
+cd /home/orchidee04/yangsu/IGCM_OUT/OL2/DEVT/Case-2-modified-algorithm/Calculation-current-NW-RCP85-5_5/Pixel-$k/SRF/Output/MO
+
+for n in `ls|tail -1`; do cp $n /home/orchidee04/yangsu/IGCM_OUT/OL2/DEVT/Case-2-modified-algorithm/Calculation-current-NW-RCP85-5_5/Final-total_pixel-year-5_5; done
+
+for n in `ls|head -1`; do cp $n /home/orchidee04/yangsu/IGCM_OUT/OL2/DEVT/Case-2-modified-algorithm/Calculation-current-NW-RCP85-5_5/Final-total_pixel-year-5; done
+
+done
